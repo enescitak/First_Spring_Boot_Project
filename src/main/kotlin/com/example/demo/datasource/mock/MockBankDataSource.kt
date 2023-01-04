9464c74bd0d61c414261ea0f9e597f145343381a
Enes Çıtak <enescitak@192.168.1.41>
2023-01-04 17:34:12 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/demo/datasource/mock/MockBankDataSource.kt
@@ -0,0 +1,19 @@
+package com.example.demo.datasource.mock
+
+import com.example.demo.datasource.BankDataSource
+import com.example.demo.model.Bank
+import org.springframework.stereotype.Repository
+
+@Repository
+class MockBankDataSource : BankDataSource {
+
+    val banks = listOf(
+        Bank("1234", 3.14, 17),
+        Bank("1010", 17.0, 0),
+        Bank("5678", 0.0, 100),
+    )
+
+    override fun retrieveBanks(): Collection<Bank> = banks
+
+    override fun retrieveBank(accountNumber: String): Bank = banks.first { it.accountNumber == accountNumber }
+}
